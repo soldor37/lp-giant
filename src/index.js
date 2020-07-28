c25d49828dab643ceda1fcad07784e0fa5cadb72
soldor37 <seventhpirate@gmail.com>
2020-07-28 21:38:37 +0300

    features done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
+//modules
 import fontawesome from '@fortawesome/fontawesome-free/js/all'
+// /modules
 
+// img
+import iPhone from './assets/img/iPhone.png'
+// /img
 
+//css
 import index from './index.scss'
+// /css
