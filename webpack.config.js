e760e1f49fc8661db0d898437a922713ffca469a
soldor37 <seventhpirate@gmail.com>
2020-07-30 18:58:29 +0300

    fix path

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,8 @@ module.exports = {
     },
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/lp-giant/'
     },
     optimization: {
         splitChunks: {
